ed45d87cd32819ccf97de7f75620324f389ef9e3
Hryhorii <realgregorygrytsyniak@gmail.com>
2023-01-19 01:34:55 +0200

    test success jenkins

---
+++ src/test/java/chadbot/HelloControllerTest.java
@@ -0,0 +1,13 @@
+package chadbot;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloControllerTest {
+
+    @Test
+    void hello() {
+        assertEquals(1, 1);
+    }
+}
